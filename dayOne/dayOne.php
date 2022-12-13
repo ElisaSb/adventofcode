d8f6c46e96b00fbed1d6f3a29dc2cc895e741c8c
Elisa Sb <elisamoon3@gmail.com>
2022-12-13 13:02:54 +0100

    [DAY ONE] Done first and second part of the one day.

---
+++ dayOne/dayOne.php
@@ -0,0 +1,31 @@
+<?php
+$fileName = '../input.txt';
+
+$content = fopen($fileName, "r");
+
+while (($line = fgets($content)) !== false) {
+    $sum += $line;
+
+    if ($line == 0) {
+        if ($sum > $max) {
+            $second = $max;
+            $third = $max;
+            $max = $sum;
+        } elseif ($sum > $second) {
+            $third = $second;
+            $second = $sum;
+        } elseif ($sum > $third) {
+            $third = $sum;
+        }
+        $sum = 0;
+    }
+}
+
+echo nl2br("Max: " . $max);
+echo nl2br("\r\n second: " . $second);
+echo nl2br("\r\n third: " . $third);
+echo nl2br("\r\n_______________\r\n");
+echo nl2br('suma --> ' . ($max + $second + $third));
+
+fclose($content);
+?>
